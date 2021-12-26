900310b39378c0e42f26a6be2de3a94325903652
Wendel Lopes <wendelopes@msn.com>
2021-12-26 09:41:48 -0300

    add #update in controller companies with test

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -10,6 +10,21 @@ module Api
           render json: { errors: { message: 'Company blocked. Contact support!' } }, status: :unauthorized
         end
       end
+
+      def update
+        company = Company.find(params[:id])
+        if company.update(params_company)
+          render json: { company: company }, status: :ok
+        else
+          render json: { errors: { message: company.errors.full_messages } }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def params_company
+        params.require(:company).permit(:name, :domain)
+      end
     end
   end
 end
--- spec/requests/api/v1/companies_spec.rb
+++ spec/requests/api/v1/companies_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Companies', :focus, type: :request do
+RSpec.describe 'Companies', type: :request do
   let(:headers) do
     {
       'Accept': 'application/vnd.dipaz.v1',
@@ -25,7 +25,7 @@ RSpec.describe 'Companies', :focus, type: :request do
       it { expect(json_body[:company][:status]).to eq('active') }
     end
 
-    context 'failure' do
+    context 'failure - access not allowed' do
       let(:company) { create(:company) }
       before do
         company
@@ -35,6 +35,34 @@ RSpec.describe 'Companies', :focus, type: :request do
       it { expect(response).to have_http_status :unauthorized }
       it 'status blocked' do
         expect(json_body[:errors][:message]).to eq('Company blocked. Contact support!')
+      end
+    end
+  end
+
+  describe 'PUT /companies/:id' do
+    let(:company) { create(:company, :active) }
+    before do
+      company
+      put "/companies/#{company.id}", params: params_company.to_json, headers: headers
+    end
+
+    context 'successfully' do
+      let(:params_company) { attributes_for(:company, name: 'Osy Updated') }
+      it { expect(response).to have_http_status :ok }
+      it 'return update data json' do
+        expect(json_body[:company][:name]).to eq(params_company[:name])
+      end
+    end
+
+    context 'failure' do
+      context 'not permited parametes' do
+        let(:params_company) { attributes_for(:company, cnpj: '11.006.382/0001-87') }
+        it { expect(json_body[:company][:cnpj]).to_not eq(params_company[:cnpj]) }
+      end
+      context 'can not be blank or nil' do
+        let(:params_company) { attributes_for(:company, name: '') }
+        it { expect(json_body[:errors][:message].to_sentence).to eq("Name can't be blank") }
+        it { expect(response).to have_http_status :unprocessable_entity }
       end
     end
   end
